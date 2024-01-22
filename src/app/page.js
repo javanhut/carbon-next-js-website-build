1e4d4a01600cad3d52446ea8136a2eea238a4f1c
Javan H. <javanstorm@gmail.com>
2024-01-21 19:28:27 -0500

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-
-export default function Home() {
+'use client';
+import LandingPage from './home/page';
+import RepoPage from './repos/page';
+import { Button } from '@carbon/react';
+export default function Page() {
   return (
     <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+      <Button>Button</Button>
+      <LandingPage />
+      <RepoPage />
     </div>
-
-  )
+  );
 }
